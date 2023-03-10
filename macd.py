99538d7dc5b39983a9931cf01fe82f4083ee2d86
timschopinski <timschopinski@gmail.com>
2023-03-10 18:39:06 +0100

    full macd char

--- macd.py
+++ macd.py
@@ -1,53 +1,52 @@
+import pandas as pd
+
 from btc_data import get_data
 
 
-def calculate_macd(n1: int = 12, n2: int = 26):
+def calculate_macd_data(n1: int = 12, n2: int = 26):
     data = get_data()[['close']].copy()
     data = data.head(1000)
 
-    # Define a custom function to apply to each rolling window
+    signal_length = 9
+
     def calculate_ema(window):
         meter = 0
         denominator = 0
         n = len(window) - 1
-        # print(n)
         alpha = 2 / (n + 1)
-        # print(alpha)
+
         for index, p in enumerate(window[::-1]):
             meter += p * (1 - alpha) ** index
             denominator += (1 - alpha) ** index
-            # print(f'meter: {meter}')
-            # print(f'denominator: {denominator}')
-            # print(f'p: {p}')
         ema = meter / denominator
-        # print('*' * 40)
-        # print('\n')
-        # print(f'ema: {ema}')
         return ema
 
-    # Use .rolling() to create a rolling window of size window_size, and .apply() to apply the custom function to each window
     data['ema1'] = data['close'].rolling(n1+1).apply(calculate_ema)
     data['ema2'] = data['close'].rolling(n2+1).apply(calculate_ema)
     data['macd'] = data['ema1'] - data['ema2']
-    print(data)
-
-    # remove Nan values
     data.dropna(subset=['macd'], inplace=True)
+    data['signal'] = data['macd'].rolling(signal_length+1).apply(calculate_ema)
+    data.dropna(subset=['signal'], inplace=True)
+    return data
 
-    # print('$' * 40)
-    # print(data['ema1'])
-    # print(data['ema2'])
-    # print(data['macd'])
 
-    for index, row in data.iterrows():
-        # Get the close_sum value for the current row
-        ema1 = row.loc['ema1']
-        ema2 = row.loc['ema2']
-        # print(f'ema1: {ema1}')
-        # print(f'ema2: {ema2}')
+def get_macd(data: pd.DataFrame) -> pd.DataFrame:
+    ema12 = data['close'].ewm(span=12, adjust=False).mean()
+    ema26 = data['close'].ewm(span=26, adjust=False).mean()
 
-    return data['macd']
+    macd = ema12 - ema26
+    signal = macd.ewm(span=9, adjust=False).mean()
+
+    data['ema12'] = ema12
+    data['ema26'] = ema26
+    data['macd'] = macd
+    data['signal'] = signal
+
+    return data
 
 
 if __name__ == '__main__':
-    calculate_macd()
+    data = get_macd(get_data().head(100).copy())
+    print(data)
+    print(data['macd'])
+    print(data['signal'])
